89a6e72fb206870b809d3c820104089ad277d81c
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-02 09:29:34 +0200

    init commit

--- InheritanceMiniProject/Program.cs
+++ InheritanceMiniProject/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
 
+// Mini project to use inheritance and interfaces
+
+namespace InheritanceMiniProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+
+    public class InventoryItemModel
+    {
+        public string ProductName { get; set; }
+    }
+
+    public class VehicleModel : InventoryItemModel
+    {
+        public decimal DealerFee { get; set; }
+    }
+
+    public class BookModel : InventoryItemModel
+    {
+        public int NumberOfPages { get; set; }
+    }
+
+}
